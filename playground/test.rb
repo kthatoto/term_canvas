366b96cf7c052d9fe7b7927819fea531f27a03b8
kthatoto <kthatoto@gmail.com>
2019-02-21 02:41:16 +0900

    Added example

--- playground/test.rb
+++ playground/test.rb
@@ -1,14 +1,30 @@
 require 'term_canvas'
-require 'json'
 
 Color = TermCanvas::Color
 main_field = TermCanvas.new(x: 0, y: 0, w: 50, h: 30)
 
+text_pos = {y: 0, x: 0}
 loop do
   key = TermCanvas.gets
+  case key
+  when ?k
+    text_pos[:y] -= 1
+  when ?j
+    text_pos[:y] += 1
+  when ?h
+    text_pos[:x] -= 2
+  when ?l
+    text_pos[:x] += 2
+  when ?q
+    break
+  end
+  main_field.clear
+  main_field.object_clear
   main_field.text(
-    x: 10, y: 10, body: "test",
+    x: text_pos[:x], y: text_pos[:y], body: "test",
     background_color: Color::RED, foreground_color: Color::BLUE
   )
-  main_field.draw
+  main_field.update
+  sleep 0.01
 end
+TermCanvas.close_all
