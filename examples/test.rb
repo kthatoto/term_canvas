173f3027f4820930bde9172dbfb9b32c3e843438
kthatoto <kthatoto@gmail.com>
2019-06-10 17:34:13 +0900

    Edit example

--- examples/test.rb
+++ examples/test.rb
@@ -19,13 +19,15 @@ loop do
   end
 
   field.clear
-  # field.text(
-  #   x: text_pos[:x], y: text_pos[:y], body: "test",
-  #   background_color: {r: 800, g: 800, b: 800}, foreground_color: {r: 200, g: 200, b: 200}
-  # )
-  rect = field.rect(
+  field.rect(
     Rect.new(
-      x: 0, y: 0, width: 10, height: 10, background_color: {r: 200, g: 800, b: 200}
+      x: 0, y: 0, width: 10, height: 10, background_color: {r: 200, b: 200, g: 800}
+    )
+  )
+  field.text(
+    Text.new(
+      x: text_pos[:x], y: text_pos[:y], body: "test",
+      background_color: {r: 800, g: 800, b: 800}, foreground_color: {r: 200, g: 200, b: 200}
     )
   )
   field.update
