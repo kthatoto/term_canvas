5d1ad734848a6a5c1b3fda0d7b26a66dda237966
kthatoto <kthatoto@gmail.com>
2019-06-25 02:48:22 +0900

    Add offset method to object

--- lib/term_canvas/object.rb
+++ lib/term_canvas/object.rb
@@ -6,5 +6,14 @@ module TermCanvas
     def set_index(index)
       @object_index = index
     end
+
+    # @param x [Integer] Offset of x
+    # @param y [Integer] Offset of y
+    # @return self
+    def offset(x:, y:)
+      @x += x
+      @y += y
+      self
+    end
   end
 end
